c7894956a8e9c0cf6be6f58f701c4ae0bffaea49
Snehal Mastud <smastud0@gmail.com>
2026-04-02 18:44:15 +0530

    Create solution.java

---
+++ Maximum Product of Splitted Binary Tree/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    private long ans;
+    private long s;
+
+    public int maxProduct(TreeNode root) {
+        final int mod = (int) 1e9 + 7;
+        s = sum(root);
+        dfs(root);
+        return (int) (ans % mod);
+    }
+
+    private long dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        long t = root.val + dfs(root.left) + dfs(root.right);
+        if (t < s) {
+            ans = Math.max(ans, t * (s - t));
+        }
+        return t;
+    }
+
+    private long sum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return root.val + sum(root.left) + sum(root.right);
+    }
+}
